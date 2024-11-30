b554ab46b4b55f7ffde8e4b88cc6c63ef9b8e94f
= <=>
2024-11-30 19:25:23 +0530

    LoginPageUpdatedCode

--- Billingpos/settings.py
+++ Billingpos/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Billingpos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,"Component"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
